3ee73330f4491219c11054cb6afffa45f58ee8bc
Peter Seale <peter@pseale.com>
2020-12-05 11:59:27 -0600

    extend line width to human-friendly levels

--- _template/prettier.config.js
+++ _template/prettier.config.js
@@ -3,4 +3,5 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: false,
+  printWidth: 200, // I hate it when prettier tries to shorten long lines. LET ME CHOOSE. Love you, Prettier, but hate how you shrink lines
 };
--- _template/src/sketch.js
+++ _template/src/sketch.js
@@ -14,16 +14,8 @@ let sketch = (p5) => {
   };
 
   p5.draw = () => {
-    // p5.clear();
-    p5.fill(
-      p5.color(150 + p5.random(100), 150 + p5.random(100), 150 + p5.random(100))
-    );
-    p5.rect(
-      p5.random(width - 50),
-      p5.random(height - 50),
-      p5.random(50),
-      p5.random(50)
-    );
+    p5.fill(p5.color(150 + p5.random(100), 150 + p5.random(100), 150 + p5.random(100)));
+    p5.rect(p5.random(width - 50), p5.random(height - 50), p5.random(50), p5.random(50));
   };
 };
 
